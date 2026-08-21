751c656276d39cab9227e64513a0c5dc68f961ae
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-08-21 22:10:18 +0530

    feat: add ApiError, ApiResponse, and asyncHandler utility classes

---
+++ server/src/utils/asyncHandler.ts
@@ -0,0 +1,11 @@
+import type { Request, Response, NextFunction } from "express"
+
+const asyncHandler = (requestHandler: Function) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        Promise
+        .resolve(requestHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+export { asyncHandler}
