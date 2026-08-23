af6b9358b16a0258ce98869bcd289a5794ce2152
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-08-24 03:21:02 +0530

    feat: add Prettier configuration and integrate with project dependencies

--- server/src/middlewares/multer.middleware.ts
+++ server/src/middlewares/multer.middleware.ts
@@ -1,7 +1,16 @@
+import type { Request } from "express"
 import multer from "multer"
 
-const upload = multer({
-    storage: multer.memoryStorage(),
-});
+const storage = multer.diskStorage({
+    destination: function(req: Request, file: Express.Multer.File,cb: Function){
+        cb(null, "./public/temp")
+    },
+    filename: function(req: Request, file: Express.Multer.File, cb: Function){
+        cb(null, file.originalname)
+    }
+})
 
-export { upload }
+
+export const upload = multer({
+    storage,
+})
