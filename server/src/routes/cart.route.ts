36ba6946569ad15473c6339628242a03ddb3935e
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-09-01 04:31:52 +0530

    feat: update cart routes to include all CRUD operations for cart management

--- server/src/routes/cart.route.ts
+++ server/src/routes/cart.route.ts
@@ -1,11 +1,13 @@
 import {  Router} from "express"
-import { addToCart, getCartItems } from "../controllers/cart.controller.js"
+import { addToCart, clearCart, getCartItems, removeFromCart, updateCart } from "../controllers/cart.controller.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router =  Router()
 
-router.post("/add-to-cart", verifyJWT, addToCart)
-router.get("/all-cart-items", verifyJWT, getCartItems)
-
+router.post("/add-to-cart", verifyJWT, addToCart);
+router.get("/all-cart-items", verifyJWT, getCartItems);
+router.patch("/update-cart/:itemId", verifyJWT, updateCart);
+router.delete("/remove/:itemId", verifyJWT, removeFromCart)
+router.delete("/clear-cart", verifyJWT, clearCart)
 
 export default router
