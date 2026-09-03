a63852dadcddf154c6f7a650d2aade5d7a5ac42c
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-09-04 00:37:15 +0530

    feat: add webhook and retry payment routes to payment processing

--- server/src/routes/payment.route.ts
+++ server/src/routes/payment.route.ts
@@ -1,12 +1,14 @@
 import { Router } from "express"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
-import { createPaymentOrder, verifyPayment } from "../controllers/payment.controller.js"
+import { createPaymentOrder, razorpayWebhook, retryPayment, verifyPayment } from "../controllers/payment.controller.js"
 
 const router = Router()
 
 
 router.post("/create", verifyJWT, createPaymentOrder)
 router.post("/verify", verifyJWT, verifyPayment)
+router.post("webhook", razorpayWebhook)
+router.post("/retry-payment/:orderId", verifyJWT, retryPayment)
 
 
 
